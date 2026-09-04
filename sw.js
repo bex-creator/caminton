701c36968061e99c1923e7c1e64219eaffebba26
BenXL731 <bexlebeau30@students.cumberlandschools.org>
2026-09-03 20:53:58 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,21 +1,23 @@
-const CACHE_NAME = 'tracker-pro-v3';
+const CACHE_NAME = 'tracker-pro-v4';
 const urlsToCache = [
+  '/caminton/',
   '/caminton/index.html',
   '/caminton/manifest.json'
 ];
 
 self.addEventListener('install', event => {
+  self.skipWaiting();
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(urlsToCache);
-    })
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
   );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(clients.claim());
 });
 
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
+    caches.match(event.request).then(response => response || fetch(event.request))
   );
 });
