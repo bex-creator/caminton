9408008bd09aad5bf9d18fd1b5ed9e129ef53b2c
BenXL731 <bexlebeau30@students.cumberlandschools.org>
2026-09-03 20:33:36 -0400

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,23 @@
+const CACHE_NAME = 'tracker-pro-v1';
+const urlsToCache = [
+  './index.html',
+  './manifest.json'
+];
+
+// Install the service worker
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+// Fetch resources
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
